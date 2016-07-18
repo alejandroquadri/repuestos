ae6cf735b9de7a580cac08fbf51b955592726661
Alejandro Quadri <alejandroquadri@gmail.com>
2016-07-18 17:08:57 -0300

    Anda la tabla sin los filtros ni data de form correcta

--- public/js/controllers/repuestosCtrl.js
+++ public/js/controllers/repuestosCtrl.js
@@ -5,23 +5,32 @@ angular.module('RepuestosApp')
   $scope.repuestos = base;
   $scope.tabla = false;
 
+  $scope.tipoStatus = ['Pendiente','Encargado','Completo','Suspendido'];
+
   $scope.submit = function () {
     var nuevo = {
       timestamp: firebase.database.ServerValue.TIMESTAMP,
       tipoItem: $scope.formulario.tipoItem || "",
       detalle: $scope.formulario.detalle || "",
+      observacion: $scope.formulario.observacion || "",
       destino: $scope.formulario.destino || "",
       proveedor: $scope.formulario.proveedor || "",
       cantidad: $scope.formulario.cantidad || "",
       unidad: $scope.formulario.unidad || "",
       muestra: $scope.formulario.muestra || "",
-      encargado: $scope.formulario.encargado || ""
+      status: "Pendiente"
     }
     console.log(nuevo);
     $scope.repuestos.$add(nuevo)
     $scope.formulario = {}
     $scope.form.$setPristine();
     $scope.form.$setUntouched();
+  }
+
+  $scope.cambioStatus = function(repuesto) {
+    $scope.repuestos.$save(repuesto).then(function(){
+      console.log('salvado');
+    })
   }
 
 })
